252a26f1782b14d01fa254a2fc8e3b83fe77bfc1
0xPublicGoods <0xpublicgoods@campsbay.com>
2023-06-15 16:39:43 +0200

    wip

--- src/Box.sol
+++ src/Box.sol
@@ -2,10 +2,25 @@
 
 pragma solidity ^0.8.18;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
 /**
  * @title Box Contract
  * @author 0xPublicGoods
  * @notice Box Contract
  * @dev Box Contract
  */
-contract Box {}
+contract Box is Ownable {
+    uint256 private s_number;
+
+    event NumberChanged(uint256 number);
+
+    function store(uint256 newNumber) public onlyOwner {
+        s_number = newNumber;
+        emit NumberChanged(newNumber);
+    }
+
+    function getNumber() external view returns (uint256) {
+        return s_number;
+    }
+}
